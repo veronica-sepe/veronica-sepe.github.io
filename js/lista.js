88cf468697dd9db6248622dd702b660275acd792
Veronica <veronica_sepe@libero.it>
2018-06-21 17:58:50 +0200

    YouTube

--- js/lista.js
+++ js/lista.js
@@ -3,56 +3,86 @@ console.log("Pagina Lista")
     // Javascript= le proprietà(name,age,..) NON necessitano di apici
 var SERVICE_URL = "http://jsonplaceholder.typicode.com";
 var API_KEY = "AIzaSyADsJpLGHyv3kKZVCJQQ3BLjLscAl_Jjs0";
-var YOUTUBE_url = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyADsJpLGHyv3kKZVCJQQ3BLjLscAl_Jjs0";
+var YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search?part=snippet&key=" + API_KEY + "&q=";
 jQuery(document).ready(function($) {
-        //la pagina è completamente carica e jQuery è pronto!
-        //jQuery==$
+        //La pagina è completamente carica e jQuery è pronto!
+        //jQuery == $
         console.log("READY!")
-            //console.log("Arguments", arguments)
+            //console.log("arguments", arguments)
 
-
-        //$("#loadingBar").hide(); //nascondo la barra di caricamento
+        // $("#loadingBar").hide(); //nascondo la barra di caricamento
         //$("#usersTable").hide(); //nascondo la tabella
 
 
-        $("#searchBtn").click(function() {
+        $("#searchBtn")
+            .click(function() {
                 console.log("Click");
-                getUsers();
+                // getUsers();
+                var search = $("#searchInput").val();
+                getVideos(search);
             })
-            .css("color", "red")
+
 
         function getUsers() {
-            $("#loadingBar").fadeIn(); //mostro la barra di caricamento
-            $("#usersTable").fadeOut(); //mostro la tabella
-            console.log("chiamo getUsers")
 
+            $("#loadingBar").fadeIn(); //mostro il caricamento
+            $("#emptyContent").fadeOut(); //tolto il contenuto iniziale vuoto
+
+            console.log("chiamo getUsers");
             $.getJSON(SERVICE_URL + "/users", function(response) {
                 var users = response;
-                console.log("Users", response)
+                console.log("USERS", users);
                 fillTable(users);
             })
         }
 
-        function fillTable(arrayData) {
-            var $tableBody = $("#usersTable tbody")
-            $tableBody.html("")
-            $.each(arrayData, function(index, user) {
-                    console.log(index, user)
-                        //creo una nuova riga vuota
-                    var newRow = jQuery("<tr/>")
-                        //inserisco dentro la riga vuota un tag td con il valore che voglio <td>VALORE</td>
-                    newRow.append("<td>" + user.id + "</td>"); //id
-                    newRow.append("<td>" + user.name + "</td>"); //name
-                    newRow.append("<td>" + user.email + "</td>"); //email
-                    newRow.append("<td>" + user.username + "</td>"); //username
-                    $tableBody.append(newRow)
-                })
-                //ritardo manualmente solo per il test
+        function getVideos(search) {
 
-            setTimeout(function() {
-                $("#loadingBar").hide();
-                $("#usersTable").show();
-            }, 2 * 1000)
+            $("#loadingBar").fadeIn(); //mostro il caricamento
+            $("#emptyContent").fadeOut(); //tolto il contenuto iniziale vuoto
+
+
+            console.log("chiamo getVideos");
+            $.getJSON(YOUTUBE_URL + search, function(response) {
+                var videos = response.items;
+                console.log("VIDEOS", videos);
+                fillTable(videos);
+            })
+        }
+
+        function fillTable(arrayData) {
+
+            var $tableBody = $("#usersTable tbody")
+
+            $tableBody.html(""); //ripulisco il contenuto del tbody
+
+            $.each(arrayData, function(index, video) {
+                    console.log(index, video);
+
+                    //creo una nuova riga vuota
+                    var newRow = jQuery("<tr></tr>");
+                    //Inserisco dentro la riga vuota un tag td con il valore che voglio <td>VALORE</td>
+                    newRow.append("<td>" + video.id.videoId + "</td>"); //id
+                    newRow.append("<td>" + video.snippet.title + "</td>") //name
+                    newRow.append("<td><img src='" + video.snippet.thumbnails.default.url + "'/></td>") //email
+
+
+                    //append la riga alla tabella
+                    $tableBody.append(newRow)
+
+                })
+                //ha riempito la tabella con tutte le righe;
+
+
+            //ritardo MANUALMENTE SOLO per TEST, 
+            setTimeout(
+                function() {
+                    $("#loadingBar").fadeOut(); // faccio scomparire il loading
+                    $("#usersTable").fadeIn(); // mostro la tabella
+                },
+                2 * 1000 //tempo in millisendi;
+            )
+
         }
 
 
@@ -61,5 +91,6 @@ jQuery(document).ready(function($) {
 
 
 
-    }) //Fine Ready
-console.log("Fine lista...!")
+
+
+    }) //Fine ready
