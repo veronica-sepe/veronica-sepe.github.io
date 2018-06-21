b4defd39749d174364f6e3c6d485cda4fb3d1318
Veronica <veronica_sepe@libero.it>
2018-06-21 14:47:59 +0200

    lista pausa pranzo

--- js/lista.js
+++ js/lista.js
@@ -1,22 +1,42 @@
 console.log("Pagina Lista")
     //oggetto Json= le proprietà(name,age,..) necessitano di apici
     // Javascript= le proprietà(name,age,..) NON necessitano di apici
-var SERVICE_URL = "http://jsonplaceholder.typicode.com/";
-var Person = {
-    "name": "Pippo",
-    "age": 24,
-    "children": [{
-            "name": "ginetto",
-            "age": 4,
-        },
-        {
-            "name": "pipino",
-            "age": 4
+var SERVICE_URL = "http://jsonplaceholder.typicode.com";
+jQuery(document).ready(function($) {
+        //la pagina è completamente carica e jQuery è pronto!
+        //jQuery==$
+        console.log("READY!")
+            //console.log("Arguments", arguments)
+
+        $("#searchBtn").click(function() {
+                console.log("Click");
+                getUsers();
+            })
+            .text("Ciao bello!")
+            .css("color", "red")
+            .click();
+
+        function getUsers() {
+            console.log("chiamo getUsers")
+
+            $.getJSON(SERVICE_URL + "/users", function(response) {
+                var users = response;
+                console.log("Users", response)
+                fillTable(users);
+            })
         }
-    ],
-    "partner": {
-        "name": "Bilbo",
-        "age": 25,
-    }
-}
-console.log(Person.name, Person.children[0].name)
+
+        function fillTable(users) {
+            $.each(users, function(index, user) {
+                console.log(index, user)
+            })
+        }
+
+
+
+
+
+
+
+    }) //Fine Ready
+console.log("Fine lista...!")
